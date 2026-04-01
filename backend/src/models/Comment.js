39e8d447ca5d7147eb23720d11072b76c907130b
Suad Abrar <suadabrar44@gmail.com>
2026-04-01 13:09:05 +0300

    apply comment model design

---
+++ backend/src/models/Comment.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 300,
+    },
+    parentComment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Comment", commentSchema);
